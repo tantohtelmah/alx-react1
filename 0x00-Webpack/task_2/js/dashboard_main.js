7d5dc0493376773563d6765250734ce6c69351d4
tantohtelmah <tantohtelmah@gmail.com>
2024-08-19 21:31:32 -0400

    tasks

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+function addParagraphs() {
+  const paragraph1 = $('<p>').text('Holberton Dashboard');
+  const paragraph2 = $('<p>').text('Dashboard data for the students');
+  const button1 = $('button').text('Click here to get started')
+  const paragraph3 = $('<p>').attr('id', 'count')
+  const paragraph4 = $('<p>').text('Copyright - Holberton School');
+
+  $('body').append(paragraph1, paragraph2, button1, paragraph3, paragraph4);
+}
+
+addParagraphs();
+
+$(document).ready(function (){
+	function updateCounter(){
+		let count = 0;
+		$('#count').text(`${count} clicks on the button`)
+	}
+	
+	const debounceUpdateCounter = debounce(updateCounter, 300);
+	
+	$('button').on('click', debounceUpdateCounter);
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js', // Your entry point (adjust the path if needed)
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'), // Output directory
+  },
+  mode: 'production', // Set mode to production
+  plugins: [
+    new HtmlWebpackPlugin({template: './public/index.html',}), // Add this line
+  ],
+  optimization: {
+    minimizer: [
+        new ImageMinimizerPlugin({
+            minimizer: [
+              'imagemin-mozjpeg',
+            ],
+        }),
+    ],
+  },
+};
